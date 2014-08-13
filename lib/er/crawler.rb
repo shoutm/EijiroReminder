da9ba69aca65f95a2a2c588eb6e8cb29e908bb85
shoutm <shouta.morimoto@gmail.com>
2014-08-13 06:29:37 +0000

    Changed the interface of parse_words_and_tags and refactoring.
    Split Er::Parser from Er::Crawler.

--- lib/er/crawler.rb
+++ lib/er/crawler.rb
@@ -1,7 +1,5 @@
 # encoding: UTF-8
 require 'open-uri'
-require 'nokogiri'
-require 'sanitize'
 require 'net/http'
 
 module Er
@@ -38,9 +36,8 @@ module Er
       nil
     end
 
-    def scrape_and_save(page_url)
+    def parse_and_save(page_url, html)
       user = Er::User.find_by_email(@id)
-      html = fetch_page(page_url)
       parser = Parser.new(html)
       word_and_tags = parser.parse_word_and_tags
 
@@ -84,73 +81,6 @@ module Er
       end
 
       super
-    end
-
-    public
-
-    class Parser
-      # --------------------------------------
-      # Definition of class instance variables
-      # --------------------------------------
-      class << self
-        attr_accessor :wordbk_table_selector, :word_id_prefix, :tag_id_prefix
-      end
-
-      @wordbk_table_selector = 'table[data-resizable-columns-id=table-wordbk]'
-      @word_id_prefix = 'word_text_'
-      @tag_id_prefix = 'js_bkid_'
-
-      # --------------------------------------
-      # Definition of instance variables
-      # --------------------------------------
-      attr_accessor :html, :doc
-
-      def initialize(html)
-        @html = html
-        @doc = Nokogiri::HTML(html)
-      end
-
-      def parse_word_and_tags()
-        word_and_tags = {}
-        words_with_ids = _get_words_with_id(@doc)
-        tags_with_ids = _get_tags_from_ids(words_with_ids.keys)
-        words_with_ids.keys.each do |id|
-          word_and_tags[id] = {}
-          word_and_tags[id]['word'] = words_with_ids[id]
-          word_and_tags[id]['tags'] = tags_with_ids[id]
-        end
-        return word_and_tags
-      end
-
-      private
-
-      def _get_words_with_id(doc)
-        wordbk_table = doc.css(Parser.wordbk_table_selector)
-        words_with_ids = {}
-        wordbk_table.css("a[id^=#{Parser.word_id_prefix}]").each do |elm|
-          id = _pick_id_from_word_text(elm.attr('id'))
-          words_with_ids[id] = elm.text
-        end
-        return words_with_ids
-      end
-
-      def _get_tags_from_ids(ids)
-        tags_with_ids = {}
-        ids.each do |id|
-          tags = []
-          tag_spans = doc.css("td[id=#{Parser.tag_id_prefix}#{id}] span")
-          tag_spans.each do |tag_span|
-            tags << Sanitize.clean(tag_span.to_s)
-          end
-          tags_with_ids[id] = tags
-        end
-        return tags_with_ids
-      end
-
-      def _pick_id_from_word_text(word_text)
-        word_text.match(/#{Parser.word_id_prefix}(\d+)/)
-        return $1
-      end
     end
   end
 end
--- spec/lib/er/crawler_spec.rb
+++ spec/lib/er/crawler_spec.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 require 'spec_helper'
 require 'yaml'
-require "#{File.dirname(__FILE__)}/crawler_spec_helper"
+require "#{File.dirname(__FILE__)}/common_spec_helper"
 
 describe 'Unit tests for Er::Crawler' do
   before :all do
@@ -88,35 +88,11 @@ describe 'Unit tests for Er::Crawler' do
   end
 
   it 'fetches all wordbook(ej) pages' do
+
   end
-
-  describe 'Parser' do
-    before :each do
-      html = @crawler.fetch_page(@wordbook_ej_url)
-      @parser = Er::Crawler::Parser.new(html)
-      @expected_words_and_tags =
-        @sample_data['wordbook_pages']['1']['words_and_tags']
-    end
-
-    it 'parses and returns words and tags related to the words' do
-      words_and_tags = @parser.parse_word_and_tags
-      if @config['fakeweb_enable']
-        expect(words_and_tags).to eq @expected_words_and_tags
-      else
-        words_and_tags.keys.each do |id|
-          expect(words_and_tags[id]['word'].class).to eq String
-          expect(words_and_tags[id]['tags'].class).to eq Array
-        end
-      end
-    end
-
-    it 'returns an error when a parse failed' do
-    end
-  end
-
 end
 
-describe 'Integration tests for Er::Crawler' do
+describe 'Integration tests for Er::Crawler and Er::Parser' do
   before :all do
     initialize_variables
     initialize_database
@@ -134,7 +110,7 @@ describe 'Integration tests for Er::Crawler' do
     else
       user = Er::User.find_by_email(@default_user.email)
       html = @crawler.fetch_page(@wordbook_ej_url)
-      parser = Er::Crawler::Parser.new(html)
+      parser = Er::Parser.new(html)
       @expected_words_and_tags = parser.parse_word_and_tags
     end
   end
@@ -143,7 +119,8 @@ describe 'Integration tests for Er::Crawler' do
     context 'with no correspondent entries in DB' do
       before(:all) do
         # No db entries before scraping
-        @crawler.scrape_and_save(@wordbook_ej_url)
+        html = @crawler.fetch_page(@wordbook_ej_url)
+        @crawler.parse_and_save(@wordbook_ej_url, html)
       end
 
       it 'stores new entries in er_items table' do
@@ -164,7 +141,8 @@ describe 'Integration tests for Er::Crawler' do
         create(:default_items_users_tag)
         Timecop.freeze
         @scraping_time = Time.now
-        @crawler.scrape_and_save(@wordbook_ej_url)
+        html = @crawler.fetch_page(@wordbook_ej_url)
+        @crawler.parse_and_save(@wordbook_ej_url, html)
         Timecop.return
       end
 
